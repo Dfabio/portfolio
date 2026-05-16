4281c698f19faa7004ee89d5ac10a2b267f8c83a
dilis <dilisofabio@gmail.com>
2026-05-16 11:04:26 +0200

    Creazione e stilizzazione banner prrincipale

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./assets/img/logo.svg";
 import "./App.css";
 import { NavBar } from "./components/Navbar/NavBar";
+import {Banner} from "./components/Banner/Banner";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner/>
     </div>
   );
 }

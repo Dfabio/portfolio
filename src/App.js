fc5e75b2ff9cdeb02adf215593d15e2cb7f95834
dilis <dilisofabio@gmail.com>
2026-05-19 12:51:11 +0200

    skillSection finita

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from "./assets/img/logo.svg";
 import "./App.css";
 import { NavBar } from "./components/Navbar/NavBar";
-import {Banner} from "./components/Banner/Banner";
+import { Banner } from "./components/Banner/Banner";
+import { SkillSection } from "./components/SkillSection/SkillSection";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Banner/>
+      <Banner />
+      <SkillSection />
     </div>
   );
 }

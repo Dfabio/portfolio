5dffd07d071ff9a83d5b84b7105c178bd54b0d6f
dilis <dilisofabio@gmail.com>
2026-05-27 15:20:05 +0200

    project, da fare la parte grafica ma component ok

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,10 @@
-import { Container,Tab, Row, Col, Nav } from "react-bootstrap";
+import { Container, Tab, Row, Col, Nav } from "react-bootstrap";
 import projImg1 from "../../assets/img/project-img1.png";
 import projImg2 from "../../assets/img/project-img2.png";
 import projImg3 from "../../assets/img/project-img3.png";
+import colorSharp2 from "../../assets/img/color-sharp2.png";
+import { ProjectCard } from "./ProjectCard";
+import './Projects.css';
 
 export const Projects = () => {
   const projects = [
@@ -45,24 +48,37 @@ export const Projects = () => {
             <h2>PROJECTS</h2>
             <p>DESCRIZIONE</p>
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
-            <Nav variant="pills" defaultActiveKey="/home">
-              <Nav.Item>
-                <Nav.Link eventKey="first">Tab One</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="second">Tab Two</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="third" >Tab Three</Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <Tab.Content>
-                
-            </Tab.Content>
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">Lorem Ipsum</Tab.Pane>
+                <Tab.Pane eventKey="third"></Tab.Pane>
+              </Tab.Content>
             </Tab.Container>
           </Col>
         </Row>
       </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
     </section>
   );
 };
